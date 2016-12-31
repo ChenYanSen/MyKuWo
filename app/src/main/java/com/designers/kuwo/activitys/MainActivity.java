3ce6cadf2076bac86a59ba0b03fd16acf9991074
ChenYanSen <1140169017@qq.com>
2017-01-01 00:35:45 +0800

    cys

--- app/src/main/java/com/designers/kuwo/activitys/MainActivity.java
+++ app/src/main/java/com/designers/kuwo/activitys/MainActivity.java
@@ -1,19 +1,22 @@
 package com.designers.kuwo.activitys;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.designers.kuwo.R;
+import com.designers.kuwo.utils.T;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     @Override
