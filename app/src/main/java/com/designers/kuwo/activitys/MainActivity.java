c25f5dcbe89d1b97f4f90cc3813babd44c4a602c
ChenYanSen <1140169017@qq.com>
2017-01-01 00:44:50 +0800

    cys

--- app/src/main/java/com/designers/kuwo/activitys/MainActivity.java
+++ app/src/main/java/com/designers/kuwo/activitys/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+    T.showLong(MainActivity.this,"ÄãºÃ2107");
     }
 
     @Override
